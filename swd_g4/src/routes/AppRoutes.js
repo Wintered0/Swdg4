63dfb0e1e468fa9cc2bbcc3c1ca7a68a9baaeb0d
hgduong <duong1701.work@gmail.com>
2025-11-09 23:12:21 +0700

    home

--- swd_g4/src/routes/AppRoutes.js
+++ swd_g4/src/routes/AppRoutes.js
@@ -1,1 +1,18 @@
-// Điều hướng
+// src/routes/index.jsx
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/HomePage";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+
+export default function AppRoutes() {
+  return (
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </MainLayout>
+  );
+}
